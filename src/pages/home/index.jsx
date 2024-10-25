b97b13d7e4f2fdf7c42f84c563c9b8d480b61063
Marshallb03 <157312409+Marshallb03@users.noreply.github.com>
2024-10-25 19:05:51 -0400

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,21 @@
+import React from 'react';
+import './index.css';
+import SearchIcon from './search.svg';
+import StateParkLogo from './Arrowhead.png';
+
+
+
 export const Home = () => {
-    return <div>Hello world this is home.</div>;
+    return (
+        <header className="App-header">
+        
+            <h1>Welcome to Our Park Finder</h1>
+            <img src={StateParkLogo} alt="logo" />
+
+            <div className="search">
+                <input type="text" placeholder="Search parks..." />
+                <img src="SearchIcon" alt="Search Icon" />
+            </div>
+        </header>
+    );
 };
