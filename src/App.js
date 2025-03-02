b62dd7094411477ec4d67a1b23da034eb45d56e4
Gavin McDavitt <80462428+gavinmcdavitt@users.noreply.github.com>
2025-03-01 20:19:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   ZeroDBScheduled();
   return (
     <div className="App">
-      <Router>
+      <Router basename="/StateParkApp">
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/Home" exact element={<Home />} />
