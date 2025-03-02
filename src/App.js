f2e619c8e34330838a8cde3af05108082a8e0222
gavin Mcdavitt <Gavin.mcdavitt@gmail.com>
2025-03-01 20:38:04 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from 'react'; // Import React hooks
 import { Home } from './pages/home/index';
 import { AboutUs } from './pages/about-us';
